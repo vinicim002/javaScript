075757a1a7a0b74ec096120c873ad0b46ce70dc4
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-07-25 18:17:02 -0300

    Dados01 e 02

---
+++ secao 003/004dados.js
@@ -0,0 +1,12 @@
+let preco = 19.90;
+let desconto = 0.4;
+let precComDesconto = preco * (1 - desconto);
+
+console.log(precComDesconto);
+
+let nome = "Caderno";
+let categoria = "Papelaria";
+
+console.log("Produto: " + nome 
+    + ", Categoria: " 
+    + categoria);
