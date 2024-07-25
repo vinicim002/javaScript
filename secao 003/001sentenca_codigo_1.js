5cf8ce94a807fa38ee20b4860cd14ae80debfccb
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-07-25 14:14:11 -0300

    Primeiros passos

---
+++ secao 003/001sentenca_codigo_1.js
@@ -0,0 +1,6 @@
+/*1a Sentenca de codigo*/
+console.log("Bom dia!");
+/*2a Sentenca de codigo*/
+console.log("Boa tarde!");
+/*3a Sentenca de codigo*/
+console.log("Boa noite!");
---
+++ secao 003/002bloco_codigo.js
@@ -0,0 +1,13 @@
+/*Bloco de codigo 1*/ 
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+/*Bloco de codigo 1*/ 
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+
